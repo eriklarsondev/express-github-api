099bd2df14691dcc3f85e5a133452beeb771b007
Erik Larson <eriklarsonco@gmail.com>
2024-09-18 08:07:07 -0400

    setup default route and added ejs files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import colors from 'colors'
 import 'dotenv/config'
 
@@ -9,6 +10,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
+
+app.use('/www', express.static(path.join(__dirname, '..', 'public')))
+
+app.get('*', (req, res) => {
+  res.status(200).render('index')
+})
 
 app.listen(port, () => {
   console.log(`\napp is running on port ${colors.bold(port)}\n`.cyan)
