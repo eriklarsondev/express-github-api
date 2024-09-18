5c3ecb71f17b67caeb8d9b68f93db3a90f7d46b6
Erik Larson <eriklarsonco@gmail.com>
2024-09-18 08:00:35 -0400

    setup main app and implemented build process

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import colors from 'colors'
+import 'dotenv/config'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.set('view engine', 'ejs')
+
+app.listen(port, () => {
+  console.log(`\napp is running on port ${colors.bold(port)}\n`.cyan)
+})
